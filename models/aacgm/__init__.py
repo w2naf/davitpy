0891e908a52759b800d276b83df9768fc0e923f8
Nathaniel A. Frissell <nafrissell@vt.edu>
2014-11-02 20:36:23 -0500

    first working test of aacgm2_convert

--- models/aacgm/__init__.py
+++ models/aacgm/__init__.py
@@ -40,26 +40,34 @@ def aacgm2_convert(in_lat, in_lon, height, dtime, flg=0):
     if (flgs.size != in_lats.size) and (flgs.size == 1):
         flgs = flgs.repeat(in_lats.size)
     
+    #Make sure things actually come out as lists.
+    if in_lats.shape == (): in_lats.shape = (1,)
+    if in_lons.shape == (): in_lons.shape = (1,)
+    if heights.shape == (): heights.shape = (1,)
+    if dtimes.shape  == (): dtimes.shape  = (1,)
+    if flgs.shape    == (): flgs.shape    = (1,)
+
     # Now put things into lists to iterate through C call...
     lat_list    = in_lats.tolist()
     lon_list    = in_lons.tolist()
-    height_list = in_heights.tolist()
-    dtime_list  = in_dtimes.tolist()
-    flg_list    = in_flgs.tolist()
+    height_list = heights.tolist()
+    dtime_list  = dtimes.tolist()
+    flg_list    = flgs.tolist()
 
     # Initialize lists for final answers and call AACGM function.
     mlats, mlons, rs = [], [], []
     current_datetime = None
+
     for lat_0, lon_0, height_0, dt_0, flg_0 in zip(lat_list,lon_list,height_list,dtime_list,flg_list):
         if dt_0 != current_datetime:
-            aacgm2.AACGM_v2_SetDateTime(dt_0.year,dt_0.month,dt_0.day,dt_0.hour,dt_0,minute,dt_0.second)
+            aacgm2.AACGM_v2_SetDateTime(dt_0.year,dt_0.month,dt_0.day,dt_0.hour,dt_0.minute,dt_0.second)
             current_datetime = dt_0
 
-        mlat_0, mlon_0, r_0 = aacgm2.AACGM_v2_Convert(lat_0,lon_0,height_0,flag_0)
+        mlat_0, mlon_0, r_0 = aacgm2.AACGM_v2_Convert(lat_0,lon_0,height_0,flg_0)
 
         mlats.append(mlat_0)
         mlons.append(mlon_0)
-        rs.append(rs)
+        rs.append(r_0)
 
     # Return a scalar if only one value
     # Return a numpy array if a numpy array was given.
@@ -73,3 +81,34 @@ def aacgm2_convert(in_lat, in_lon, height, dtime, flg=0):
         ret_rs      = np.array(rs)
 
     return ret_mlats,ret_mlon,ret_rs
+
+if __name__ == '__main__':
+    import unittest
+    from models import aacgm
+
+    class DavitPyAacgmTest(unittest.TestCase):
+        def setUp(self):
+            self.accuracy = 6
+
+            dt = datetime.datetime
+            self.test_data = []
+            #                      (     glat,       glon,      height,             datetime, flg,      mlat,       mlon,        r,      mlt,      mslong)
+            self.test_data.append( (45.500000, -23.500000, 1135.000000, dt(2014, 3,22, 3,11),   0, 48.377539,  57.822458, 1.000000, 2.092153, -153.559832) )
+            self.test_data.append( (45.500000, -23.500000, 1135.000000, dt(1997, 3,22, 3,11),   0, 49.425800,  58.259686, 1.000000, 2.121862, -153.568237) )
+            self.test_data.append( (65.500000,  93.500000, 1135.000000, dt(1997, 3,22, 3,11),   0, 62.251076, 166.990581, 1.000000, 9.370588, -153.568237) )
+            self.test_data.append( (65.500000,  93.500000,           0, dt(1997, 3,22, 3,11),   0, 60.799240, 166.518084, 1.000000, 9.339088, -153.568237) )
+            self.test_data.append( (75.500000,  73.500000,           0, dt(1997, 3,22, 3,11),   0, 70.420669, 150.743259, 1.000000, 8.287433, -153.568237) )
+            self.test_data.append( (75.500000,  73.500000,           0, dt(2004, 3,22, 3,11),   0, 70.726381, 150.672892, 1.000000, 8.338513, -154.404804) )
+
+        def test_aacgm2_convert_scalar(self):
+            print('Testing the scalar functionality of aacgm2_convert.')
+
+            for item in self.test_data:
+                glat,glon,height,dtime,flg,mlat,mlon,r,mlt,mslong = item
+
+                mlat_test, mlon_test, r_test = aacgm2_convert(glat,glon,height,dtime,flg)
+                self.assertAlmostEqual(mlat_test,mlat,self.accuracy)
+                self.assertAlmostEqual(mlon_test,mlon,self.accuracy)
+                self.assertAlmostEqual(r_test,r,self.accuracy)
+           
+    unittest.main()
